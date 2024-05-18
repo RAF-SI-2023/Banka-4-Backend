d68f1d80d77e27b3ee18f4964ba088fabcf0c017
Aleksa Buncic <alextromb1@gmail.com>
2024-05-18 20:30:32 +0200

    Sklonjen user servis

--- user-service/src/main/java/rs/edu/raf/exceptions/WrongEmployeeException.java
+++
@@ -1,9 +0,0 @@
-package rs.edu.raf.exceptions;
-
-import org.springframework.http.HttpStatus;
-
-public class WrongEmployeeException extends CustomException{
-    public WrongEmployeeException(String message) {
-        super(message, ErrorCode.WRONG_EMPLOYEE, HttpStatus.BAD_REQUEST);
-    }
-}
